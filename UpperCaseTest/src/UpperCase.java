aef0bb403b20d54b23d27951e97639e0f67d4f7d
sachitha2 <shpsachitha@gmail.com>
2019-12-10 12:27:09 +0530

    modifications

--- UpperCaseTest/src/UpperCase.java
+++ UpperCaseTest/src/UpperCase.java
@@ -6,6 +6,13 @@ public class UpperCase {
     {
         String word = JOptionPane.showInputDialog("Enter a text");
         System.out.println(word.toUpperCase());
+        //sub string
+        System.out.println(word.substring(5));
+        //index of
+        String sente = "Sachitha hirushan";
+        System.out.println("index of s "+sente.indexOf('S'));
+        System.out.println("last index of i "+sente.indexOf('i'));
+
 
     }
 }
