4d19cde329fa8cb3bafe6835ce0710f3d82a9590
sachitha2 <shpsachitha@gmail.com>
2020-01-31 21:24:33 +0530

    UI dev

--- UniManeV3/src/com/Uni/Home.java
+++ UniManeV3/src/com/Uni/Home.java
@@ -2,11 +2,13 @@ package com.Uni;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Home {
     private JPanel panel1;
-    private JButton lecturerButton;
-    private JButton studentButton;
+    private JButton lecBtn;
+    private JButton stdBtn;
 
 
     Home(){
@@ -19,6 +21,24 @@ public class Home {
 
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setSize(screenSize.width,screenSize.height);
+        stdBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("You clicked student button");
+                StudentUI studentUI = new StudentUI();
+                frame.setVisible(false);
+            }
+        });
+        lecBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("You clicked lec btn");
+                LecturerUI lecturerUI = new LecturerUI();
+                frame.setVisible(false);
+
+            }
+
+        });
     }
 
 
