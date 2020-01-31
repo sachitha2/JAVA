a67deba170b638eedb288f59c8d9b3eaabfd0b23
sachitha2 <shpsachitha@gmail.com>
2020-01-31 12:31:54 +0530

    sign up making

--- UniManeV3/src/com/Uni/RegisterForm.java
+++ UniManeV3/src/com/Uni/RegisterForm.java
@@ -3,15 +3,26 @@ package com.Uni;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
 
 public class RegisterForm extends JFrame{
     private JPanel panel1;
     private JButton loginButton;
     private JButton signUpButton;
-    private JTextField textField1;
-    private JTextField textField2;
+    private JTextField uName;
+    private JTextField pass;
+    private JTextField pass2;
 
+
+    public static DB obj_DB_Connection;
     RegisterForm(){
+
+
+
+        obj_DB_Connection=new DB();
+        Connection connection=null;
+        connection=obj_DB_Connection.get_connection();
+        System.out.println(connection);
 
         JFrame frame = new JFrame("RegisterForm");
         frame.setContentPane(this.panel1);
@@ -30,6 +41,20 @@ public class RegisterForm extends JFrame{
             @Override
             public void actionPerformed(ActionEvent e) {
                 //create sign up part and intent to home
+
+                if(pass.getText().equals(pass2.getText())){
+                    System.out.println("password match");
+                }else {
+                    JOptionPane.showMessageDialog(null, "Password does not match");
+                }
+
+                signUpObj o = new signUpObj(uName.getText(),pass.getText());
+
+                int states = obj_DB_Connection.signUp(o);
+
+
+
+
             }
         });
     }
